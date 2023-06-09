cc8d7b8a62219049561e650389dbdd3cceb410ea
ishani-21 <ishani.r@elaunchinfotech.in>
2023-06-09 15:45:52 +0530

    rediredt dashboard page when not found route

---
+++ src/jsx/pages/Common/Route.jsx
@@ -0,0 +1,25 @@
+const route = [
+    'dashboard',
+    'dashboard-dark',
+    'user-list',
+    'subscribe-user-list',
+    'payment-history',
+    'upcoming-subscriptions',
+    'view-maintence',
+    'cms-list',
+    'manage-subscriptions',
+    'faq-list',
+    'contact-us',
+    'chat',
+    'general-notification',
+    'user-notification',
+    'notification-detail',
+    'user-detail',
+    'maintenance-request-list',
+    'technical-guide-list',
+    'faq-group-list',
+    'technician-list',
+    'live-chat'
+]
+
+export default route
